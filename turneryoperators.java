b098dfeaec4b6b8907ec77bbfcc4c63c42d112c0
chandramohan010 <82196034+chandramohan010@users.noreply.github.com>
2023-05-01 14:41:36 +0530

    Add files via upload

---
+++ turneryoperators.java
@@ -0,0 +1,12 @@
+class turneryoperators1
+{
+       public static void main(String args[])
+       {          
+                 int a=2;
+                 int b=7;
+                 int x= (a>b)?a:b;
+                 System.out.println(x+" "+"is grater");
+                 
+       }
+}
+write a program to find greaer of three number using turnery operator
